8d899074f4949a2f5f5cdbb8e069d6562ba9d251
clarissagtz <cgutierrezgodoy@ucsd.edu>
2022-09-07 17:08:15 -0700

    message

--- src/screens/Ctemp.js
+++ src/screens/Ctemp.js
@@ -1,5 +1,7 @@
 import { Button } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
+//need to install react bootstrap to this file do: 
+// import Card from 'react-bootstrap/Card';
 
 
 const Ctemp = () => {
@@ -7,7 +9,7 @@ const Ctemp = () => {
     return (
         <div>
             <h2> Clarissa's page for now  </h2>
-
+            <h5>clarissaa</h5>
 
             <Button variant="btn btn-success" className="mainButton" onClick={()=> history.push("/")}>Go to index </Button>
         </div>
